4ece7a5e4c97375ef28bca7b063b4e5510e679d2
Drew Meyers <drew.meyers@jpl.nasa.gov>
2024-04-30 18:02:00 -0700

    Implementing orm and endpoints

--- app/schemas/ogc_processes.py
+++ app/schemas/ogc_processes.py
@@ -10,14 +10,10 @@ from typing import Any, Dict, List, Optional, Set, Union
 
 from pydantic import AnyUrl, BaseModel, ConfigDict, Field, PositiveFloat, RootModel, confloat, conint
 
-# from pydantic.alias_generators import to_camel
-
 
 class BaseSchema(BaseModel):
     model_config = ConfigDict(
         use_enum_values=True,
-        # from_attributes=True,
-        # alias_generator=to_camel
     )
 
 
