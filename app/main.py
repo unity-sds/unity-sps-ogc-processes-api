50b8ae7f5f457218e17a23b03938db6c9920b130
Drew Meyers <drew.meyers@jpl.nasa.gov>
2024-05-04 15:39:41 -0700

    Implementing tests

--- app/main.py
+++ app/main.py
@@ -516,7 +516,6 @@ async def landing_page():
 )
 def get_health() -> HealthCheck:
     """
-    ## Perform a Health Check
     Endpoint to perform a healthcheck on. This endpoint can primarily be used Docker
     to ensure a robust container orchestration and management is in place. Other
     services which rely on proper functioning of the API service will not deploy if this
