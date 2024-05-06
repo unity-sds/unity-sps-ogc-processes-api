3fafa1b597ff546538c9a9627b81c21fd521dd32
Drew Meyers <drew.meyers@jpl.nasa.gov>
2024-05-06 10:21:16 -0700

    Remove commented code

--- app/config.py
+++ app/config.py
@@ -1,10 +1,9 @@
-from pydantic import HttpUrl  # , PostgresDsn, AnyUrl
+from pydantic import HttpUrl
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     db_url: str = "sqlite:///:memory:"
-    # pg_dsn: PostgresDsn
     ems_api_url: HttpUrl = "http://localhost:8080/api/v1"
     ems_api_auth_username: str = "username"
     ems_api_auth_password: str = "password"
